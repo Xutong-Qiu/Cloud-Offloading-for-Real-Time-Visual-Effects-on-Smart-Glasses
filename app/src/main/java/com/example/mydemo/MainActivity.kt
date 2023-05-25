f6d1e017354cd714b4bad62fe1fab2ea86c11626
邱勖桐 <qiuxutong@wifi-131-179-19-229.host.ucla.edu>
2023-05-25 11:28:07 -0700

    backup

--- app/src/main/java/com/example/mydemo/MainActivity.kt
+++ app/src/main/java/com/example/mydemo/MainActivity.kt
@@ -208,7 +208,7 @@ class MainActivity : Activity() {
         camera.setExposure(16.0f, 1.0f / 125.0f, 100.0f)
 
         //this is the default value
-        camera.lookAt(0.0, 0.0, 5.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0)
+        camera.lookAt(-2.0, 1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
         //startAnimation()
     }
 
@@ -407,7 +407,7 @@ class MainActivity : Activity() {
         animator.repeatCount = ValueAnimator.INFINITE
         animator.addUpdateListener { a ->
             val v = (a.animatedValue as Float)
-            camera.lookAt(cos(v) * 9.0, 3.0, sin(v) * 9.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+            camera.lookAt(cos(v) * 3.0, 1.0, sin(v) * 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
         }
         animator.start()
     }
